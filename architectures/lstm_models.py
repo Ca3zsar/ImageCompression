bac61b1e45858048cbba1925c3ade4fea86d3678
Todirisca Cezar <cezartodirisca@gmail.com>
2024-04-09 15:35:38 +0300

    STEP-1: Adding LISTM Based Autoencoder

---
+++ architectures/lstm_models.py
@@ -0,0 +1,137 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+
+@tf.custom_gradient
+def binary_out(inputs):
+    binary = tf.sign(inputs)
+
+    def grad(dy):
+        return dy
+
+    return binary, grad
+
+class Encoder(tf.keras.models.Model):
+    def __init__(self, batch_size, binary_size, patch_size):
+        super(Encoder, self).__init__()
+
+        self.batch_size = batch_size
+        self.binary_size = binary_size
+        self.patch_size = patch_size
+
+        self.hidden_size = 512
+
+        self.custom_layers = [
+            layers.Dense(self.patch_size*self.patch_size*3, activation='tanh', trainable=True),
+            layers.LSTMCell(self.hidden_size, trainable=True),
+            layers.LSTMCell(self.hidden_size, trainable=True),
+        ]
+        self.custom_layers.append(tf.keras.layers.Dense(binary_size, activation=binary_out, use_bias=False, trainable=True))
+
+    def init_state(self):
+        h_1_0 = tf.zeros([self.batch_size, self.hidden_size])
+        c_1_0 = tf.zeros([self.batch_size, self.hidden_size])
+        h_2_0 = tf.zeros([self.batch_size, self.hidden_size])
+        c_2_0 = tf.zeros([self.batch_size, self.hidden_size])
+        return (h_1_0, c_1_0), (h_2_0, c_2_0)
+
+    def call(self, inputs, training=True):
+        x, state = inputs
+        
+        x = tf.reshape(x, [-1, self.patch_size*self.patch_size*3])
+        x = self.custom_layers[0](x)
+        h_out1, c_out1 = self.custom_layers[1](x, state[0])
+        h_out2, c_out2 = self.custom_layers[2](h_out1, state[1])
+        bits = self.custom_layers[3](h_out2)
+        new_state = ((h_out1, c_out1[1]), (h_out2, c_out2[1]))
+        return bits, new_state
+    
+
+    def build_graph(self):
+        x = tf.keras.layers.Input(shape=(self.patch_size , self.patch_size , 3 ))
+        return tf.keras.models.Model(inputs=[x], outputs=self.call(x))
+    
+
+class Decoder(tf.keras.models.Model):
+    def __init__(self, batch_size, binary_size, patch_size):
+        super(Decoder, self).__init__()
+
+        self.batch_size = batch_size
+        self.binary_size = binary_size
+        self.patch_size = patch_size
+
+        self.hidden_size = 512
+
+        self.custom_layers = [
+            layers.LSTMCell(self.hidden_size, trainable=True),
+            layers.LSTMCell(self.hidden_size, trainable=True),
+            layers.Dense(self.hidden_size, activation='tanh', trainable=True),
+        ]
+        self.custom_layers.append(layers.Dense(patch_size*patch_size*3, activation='tanh', trainable=True))
+
+    def init_state(self):
+        h_1_0 = tf.zeros([self.batch_size, self.hidden_size])
+        c_1_0 = tf.zeros([self.batch_size, self.hidden_size])
+        h_2_0 = tf.zeros([self.batch_size, self.hidden_size])
+        c_2_0 = tf.zeros([self.batch_size, self.hidden_size])
+        return (h_1_0, c_1_0), (h_2_0, c_2_0)
+
+    def call(self, inputs, training=True):
+        x, state = inputs
+        h_out1, c_out1 = self.custom_layers[0](x, state[0])
+        h_out2, c_out2 = self.custom_layers[1](h_out1, state[1])
+        x = self.custom_layers[2](h_out2)
+        x = self.custom_layers[3](x)
+        x = tf.reshape(x, [-1, self.patch_size, self.patch_size, 3])
+        new_state = ((h_out1, c_out1[1]), (h_out2, c_out2[1]))
+        return x, new_state
+    
+    def build_graph(self):
+        x = tf.keras.layers.Input(shape=(self.patch_size , self.patch_size , 3 ))
+        return tf.keras.models.Model(inputs=[x], outputs=self.call(x))
+    
+
+class LSTM_Autoencoder(tf.keras.models.Model):
+    def __init__(self, batch_size, binary_size, patch_size, steps):
+        super(LSTM_Autoencoder, self).__init__()
+
+        self.steps = steps
+
+        self.encoder = Encoder(batch_size, binary_size, patch_size)
+        self.decoder = Decoder(batch_size, binary_size, patch_size)
+    
+    def train_step(self, data):
+        x, y = data
+        with tf.GradientTape() as tape:
+            y_pred = self(x, training=True)
+            loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
+
+        trainable_vars = self.trainable_variables
+        gradients = tape.gradient(loss, trainable_vars)
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))  
+
+        for metric in self.metrics:
+            if metric.name == 'loss':
+                metric.update_state(loss)
+            else:
+                metric.update_state(y, y_pred) 
+
+        return {m.name: m.result() for m in self.metrics}    
+         
+    def call(self, inputs, training=True):
+        x = inputs
+        self.encoder_state = self.encoder.init_state()
+        self.decoder_state = self.decoder.init_state()
+
+        for i in range(self.steps):
+            bits, self.encoder_state = self.encoder((x, self.encoder_state))
+            x, self.decoder_state = self.decoder((bits, self.decoder_state))
+        
+            residual = x - inputs
+            self.add_loss(tf.reduce_mean(tf.square(residual)))
+            x = residual
+
+        return residual
+
+    def build_graph(self):
+        x = tf.keras.layers.Input(shape=(self.encoder.patch_size , self.encoder.patch_size , 3 ))
+        return tf.keras.models.Model(inputs=[x], outputs=self.call(x))
---
+++ metrics/ssim.py
@@ -0,0 +1,15 @@
+# Structural Similarity Index (SSIM) metric
+#
+
+# Path: metrics/ssim.py
+
+import tensorflow as tf
+
+def ssim(y_true, y_pred, max_val=1.0):
+    y_true = tf.convert_to_tensor(y_true)
+    y_pred = tf.convert_to_tensor(y_pred)
+    y_true = tf.cast(y_true, tf.float32)
+    y_pred = tf.cast(y_pred, tf.float32)
+    y_true = tf.expand_dims(y_true, 0)
+    y_pred = tf.expand_dims(y_pred, 0)
+    return tf.image.ssim(y_true, y_pred, max_val=max_val)
